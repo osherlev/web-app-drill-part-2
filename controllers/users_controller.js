ffd378c4ecc729dfab24589e16ce7d45f02a3965
osherlev <osherlev317@gmail.com>
2024-11-30 21:02:20 +0200

    feat: add tokens

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,6 @@
 const User = require("../models/users_model");
+const token = require('../utils/jwt_util');
+const bcrypt = require("bcrypt");
 
 const registerUser = async (req, res) => {
     try {
@@ -36,6 +38,31 @@ const getUserById = async (req, res) => {
     }
 };
 
+const login = async (req, res) => {
+    try {
+        const {username, password} = req.body;
+        const existingUser = await User.findOne({username});
+        const isMatchedPassword = await bcrypt.compare(password, existingUser?.password);
+        if (!isMatchedPassword) {
+            return res.status(400).json({ error: "wrong credentials. Please try again."});
+        }
+        const {accessToken, refreshToken} = await token.generateTokens(existingUser);
+        token.updateCookies(accessToken, refreshToken, res);
+        return res.status(200).json({message: "logged in successfully."});
+    } catch (err) {
+        console.warn("Error while logging in:", err);
+        return res.status(500).json({ error: "An error occurred while logging in.", err});
+    }
+}
 
+const logout = async (req, res) => {
+    try{
+        token.clearCookies(res);
+        return res.status(200).json({message: "logged out successfully."});
+    } catch {
+        console.warn("Error while logging out:", err);
+        return res.status(500).json({ error: "An error occurred while logging out.", err});
+    }
+}
 
-module.exports = {registerUser, getAllUsers, getUserById}
+module.exports = {registerUser, getAllUsers, getUserById, login, logout}
