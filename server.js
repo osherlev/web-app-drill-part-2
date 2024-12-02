b4990d94417471d08c3655ca13c260478b92de18
osherlev <osherlev317@gmail.com>
2024-12-02 19:28:11 +0200

    feat: add endpoint for swagger.yaml

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 const { connectDB } = require("./utils/connectDb");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./docs/swagger.yaml");
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -11,6 +14,7 @@ app.use(bodyParser.json());
 app.use("/post", require("./routes/posts_routes"));
 app.use("/comment", require("./routes/comments_router"));
 app.use("/user", require("./routes/users_router"));
+app.use("/swagger-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const initApp = () => {
     return new Promise(async (resolve, reject) => {
